f01d0162e4514b3343eaecef79c2bd9cdc94925a
Victoria Azola Silva <116470398+VickyAzola@users.noreply.github.com>
2024-10-09 20:49:32 -0300

    Add files via upload

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -6,6 +6,6 @@ export default {
 
 <template lang="">
   <footer class="py-5 px-5">
-    <img src="/src/assets/img/logo.png" class="mx-auto d-block" alt="suricata" />
+    <img src="/img/logo.png" class="mx-auto d-block" alt="suricata" />
   </footer>
 </template>
